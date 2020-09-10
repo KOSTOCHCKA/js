08daca910127a1e58fa93929f623d93de81606be
KOSTOCHCKA <42142192+KOSTOCHCKA@users.noreply.github.com>
2020-09-10 17:20:47 +0700

    Add files via upload

---
+++ application_programming/special_values/js/script.js
@@ -0,0 +1,28 @@
+//1.
+let a;
+alert(a); 
+
+//2.
+let a1 = null;
+alert(a1);
+
+//3.
+let a2 = true;
+alert(a2);
+
+//4.
+let a3 = false;
+alert(a3);
+
+//5.
+let b = 2;
+let c = 3;
+let d = 0;
+d = b*c;
+alert(d);
+
+//6.
+alert(10 / 0);
+
+//7.
+alert(-10 / 0);

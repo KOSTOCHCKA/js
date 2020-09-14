0161b8cb5937c02b0f109b87d73f77d1ef0c9414
KOSTOCHCKA <42142192+KOSTOCHCKA@users.noreply.github.com>
2020-09-14 16:01:19 +0700

    Add files via upload

---
+++ application_programming/types_conversion/js/script.js
@@ -0,0 +1,24 @@
+//1.
+let a = 'asced';
+let b = 3;
+alert(a[b]);
+
+//2.
+let a1 = 'asdasdasd';
+alert(a1[a1.length - 1]);
+
+//3.
+let a2 = 'asdasdasd';
+alert(a2[a2.length - 2]);
+
+//4.
+let a3 = '123456';
+alert(Number(a3[0]) + Number(a3[1]) + Number(a3[2]) + Number(a3[3]) + Number(a3[4]) + Number(a3[5]));
+
+//5.
+let a4 = '123456';
+alert(Number(a4[0]) * Number(a4[1]) * Number(a4[2]) * Number(a4[3]) * Number(a4[4]) * Number(a4[5]));
+
+//6.
+let a5 = '123456';
+alert(Number(a5[5]),Number(a5[4]),Number(a5[3]),Number(a5[2]),Number(a5[1]),Number(a5[0]),)

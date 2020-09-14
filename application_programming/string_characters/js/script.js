c97ed262abff0e50d1321f4b71a37f29acf3b528
KOSTOCHCKA <42142192+KOSTOCHCKA@users.noreply.github.com>
2020-09-14 14:44:32 +0700

    Add files via upload

---
+++ application_programming/string_characters/js/script.js
@@ -0,0 +1,39 @@
+//1.
+let a = Number('10');
+let b = Number('20');
+alert(a + b);
+
+//2.
+let a1 = Number'2';
+let b1 = Number'3';
+alert(a1 + b1);
+
+//3.
+let num = parseInt('5px')
+let num1 = parseInt('5px')
+alert(num + num1)
+
+//4.
+let num2 = parseFloat('6.25px')
+let num3 = parseFloat('5.5px')
+alert(num2 + num3)
+
+//5.
+let num4 = parseFloat('6.25px')
+let num5 = parseFloat('5.5px')
+alert(num4 + num5 + 'px')
+
+//6.
+let c = 1;
+let d = 2;
+
+alert(String(c) + String(d));
+
+//7.
+let p = 12345;
+alert(String(p).length);
+
+//8.
+let i = 12345;
+let u = 12345;
+alert(String(i).length + String(u).length);
